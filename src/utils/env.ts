f79b42a99af9cc7504044bb4da996d228aefce7f
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-08-10 14:10:51 +0700

    updated

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "897539643389373";
-export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || "3B_09teXBspD49OxcI-gNCfqAS8";
-export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || "dbot4jz71";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || "";
 
-export const DATABASE_URL: string =
-  "mongodb+srv://krisnabmntr:PmqcA6ci98MJmsT3@belajar-backend-nodejs.vzey3tq.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejs";
+export const DATABASE_URL: string = process.env.DATABASE_URL ||
+  "";
 
-export const SECRET: string = process.env.SECRET || "12345678901234567890123456789012";
+export const SECRET: string = process.env.SECRET || "";
